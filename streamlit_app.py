1280b129228ed2e3079feded54c2b99511af6533
rachanasatheli <114474916+rachanasatheli@users.noreply.github.com>
2022-10-10 11:55:28 +0530

    Update streamlit_app.py
    streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ streamlit.header('My parents healthy dinner')
 streamlit.title('breakfast menu')
 streamlit.text('hard boiled eggg')
 streamlit.text('oat meals')
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
